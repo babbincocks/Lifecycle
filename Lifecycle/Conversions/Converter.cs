38e171813b982e9d769d6e9e93891d942c9fcbd1
babbincocks <zabbybab@gmail.com>
2018-03-28 21:27:17 -0400

    Finished Roman to Arabic, still working on Arabic to Roman.

---
+++ Lifecycle/Conversions/Converter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions
+{
+    public class NumberConversion
+    {
+        private static Dictionary<char, int> RomanToInt = new Dictionary<char, int>()
+            { //This is a dictionary that will be used for Roman to Arabic. 
+            ///It shows each Roman numeral, and the value that they correspond to.
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+
+        public static string ArabicToRoman(int Arabic)
+        {
+
+            
+                string returnRoman = "";
+                   
+                if ()
+            {
+                
+            }
+
+                return returnRoman;
+            
+
+        }
+
+
+
+
+        public static int RomanToArabic(string roman)
+        {
+            //An initial variable for what will be returned at the end is declared.
+            int returnArabic = 0;
+            
+            /*
+             A for-loop is established that will create what will essentially function as
+             a cursor (hence the variable's name), and as long as the value (which works as
+             the position of the cursor) is less than the length of the Roman numeral being
+             tested, it will do everything in the loop, and then increment the cursor value.
+            */
+         for (int cursor = 0; cursor < roman.Length; cursor++)
+              {
+                /*
+                 This initial half to the if statement checks to see if the next cursor
+                 position is less than the length of the numerals being checked (since the
+                 cursor is base 0, and the actual length of the string is base 1, the only
+                 time it would not fit this criteria is when it's on the last numeral), and
+                 that the value the cursor is currently on is less than the value of the
+                 roman numeral that will come next. If both of these conditions are true,
+                 then the value that the cursor is currently on is subtracted from the
+                 current value of the return value.
+                 */
+                if (cursor + 1 < roman.Length && RomanToInt[roman[cursor]] <
+                    RomanToInt[roman[cursor + 1]])
+                {
+                    returnArabic -= RomanToInt[roman[cursor]];
+                }
+                /*
+                 If the numeral the cursor is currently on does NOT fit either of those
+                 criteria, the respective value of the numeral is added on to the return
+                 value instead. 
+                 */
+                else
+                {
+                    returnArabic += RomanToInt[roman[cursor]];
+                }
+                /*
+                The reason for the subtraction part of this is so when you get numerals
+                like, say, MDLIV, and the cursor has scanned the first three numerals 
+                (giving the return value a value of 1550), it will check if the I is not
+                the last number (fits that criteria), and that the next numeral is greater
+                than itself (which V is, so it fits that criteria as well), so it subtracts
+                I (1), and then it'll add V(5), so it works out.
+                 */
+
+            }
+            //Then it finishes with returning the value that has been added into.
+            return returnArabic;
+        }
+
+    }
+}
---
+++ Lifecycle/Conversions/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRToA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = NumberConversion.RomanToArabic(inputText.Text);
+                outputText.Text = a.ToString();
+            }
+            catch
+            {
+
+
+            }
+        }
+
+        private void btnAToR_Click(object sender, EventArgs e)
+        {
+            string a = NumberConversion.ArabicToRoman(int.Parse(inputText.Text));
+            outputText.Text = a;
+            
+        }
+    }
+
+}
