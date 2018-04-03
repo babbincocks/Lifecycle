1ab77e1f0aad2b05d6f214fcbcca1ed18ffddd6a
babbincocks <zabbybab@gmail.com>
2018-04-02 21:37:08 -0400

    Made progress on the technical specifications, and another small edit to the Converter.

--- Lifecycle/Conversions/Converter.cs
+++ Lifecycle/Conversions/Converter.cs
@@ -119,8 +119,9 @@ namespace Conversions
                 if (hasInt)
                 {
                     throw new FormatException("Please insert only valid Roman numerals (I, V, X, L, C, D, M).");
-
                 }
+
+                if (roman.Contains(""))
 
                 /*
                  A for-loop is established that will create what will essentially function as
